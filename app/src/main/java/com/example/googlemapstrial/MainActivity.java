098399d62a9fa66193022bb546188fa727340803
adhvikharikrishnan <adhvikthedude@gmail.com>
2024-03-13 11:08:12 -0700

    This is my (Adhvik) first commit! I have got the onClickListener to sort of work. What I did is I simply added markers. Aditya was attempting to use markers, but he ended up creating onClickListeners for the actual map. Adding markers and then dragging it allows for you to get the country based on the latitude and longitude coordinates. Using Aditya's structure and this onClick method, we have a good baseline to start the project.

--- app/src/main/java/com/example/googlemapstrial/MainActivity.java
+++ app/src/main/java/com/example/googlemapstrial/MainActivity.java
@@ -12,13 +12,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     private GoogleMap map;
 
-
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +29,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         mapFragment.getMapAsync(this);
 
-        double latitude = 37.313782;
+        /*double latitude = 37.313782;
         double longitude = -121.969380;
         String country = GeoCodingHelper.getCountryFromLocation(this, latitude, longitude);
-        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();*/
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
         LatLng location = new LatLng(37.313782, -121.969380);
-        googleMap.addMarker(new MarkerOptions().position(location).title("This is Stratford Prep!"));
+        marker = map.addMarker(new MarkerOptions()
+                .position(location)
+                .draggable(true)
+                .title("marker")
+        );
+
+        map.setOnMarkerDragListener(this);
+        //googleMap.addMarker(new MarkerOptions().position(location).title("This is Stratford Prep!"));
         //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,1));
         map.getUiSettings().setZoomControlsEnabled(true);
 
 
+    }
 
 
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker) {
 
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+        String country = GeoCodingHelper.getCountryFromLocation(this, marker.getPosition().latitude, marker.getPosition().longitude);
+        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker) {
 
     }
 }
