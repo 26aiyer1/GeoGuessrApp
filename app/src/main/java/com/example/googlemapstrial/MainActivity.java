e4cf47b6d90711a253477e08494d7d0048d0d961
adhvikharikrishnan <adhvikthedude@gmail.com>
2024-03-13 22:19:03 -0700

    im not sure.. maybe this is the fifth time i'm pushing this.. idk
    anyways, i have added a json that basically controls the 'front-end'
    of the map. essentially, i removed the labels and there is much more we could do.

--- app/src/main/java/com/example/googlemapstrial/MainActivity.java
+++ app/src/main/java/com/example/googlemapstrial/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.googlemapstrial;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 
@@ -12,6 +17,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -33,11 +39,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
         map = googleMap;
 
         map.setOnMapClickListener(this);
         map.getUiSettings().setZoomControlsEnabled(true);
 
+        map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
+
+        try
+        {
+            boolean success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json ) );
+
+            if( !success )
+            {
+                Log.e( TAG, "Style parsing failed." );
+            }
+        }
+        catch( Resources.NotFoundException e )
+        {
+            Log.e( TAG, "Can't find style. Error: ", e );
+        }
+
+
+
+       // map.setMapType(MapStyleOptions.loadRawResourceStyle(this, R.ra));
 
     }
 
