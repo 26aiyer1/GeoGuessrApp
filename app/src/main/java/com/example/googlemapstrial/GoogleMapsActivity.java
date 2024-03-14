d80198d3737cfd9d1817d8f63fd31a5fa9be7f7a
26aiyer1 <26aiyer1@stratfordschools.net>
2024-03-14 11:37:29 -0700

    Moved things around and made changes to the helper class

--- app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
+++ app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
@@ -1,17 +1,22 @@
 package com.example.googlemapstrial;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap map;
 
@@ -23,44 +28,38 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         mapFragment.getMapAsync(this);
     }
-    // Does not work
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
         map = googleMap;
-
-        LatLng location = new LatLng(37.313782, -121.969380);
-        Marker marker = googleMap.addMarker(new MarkerOptions().position(location).title("This is Stratford Prep!"));
-
+        map.setOnMapClickListener(this);
         map.getUiSettings().setZoomControlsEnabled(true);
+        map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
 
-        // Set a click listener for the marker
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                // Handle marker click
-                handleMarkerClick(marker);
-                return true; // Consume the event to prevent default behavior
+        try
+        {
+            boolean success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json ) );
+            if( !success )
+            {
+                Log.e( TAG, "Style parsing failed." );
             }
-        });
+        }
+        catch( Resources.NotFoundException e )
+        {
+            Log.e( TAG, "Can't find style. Error: ", e );
+        }
 
-        // Set a click listener for the map
-        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                // Handle map click
-                handleMapClick(latLng);
-            }
-        });
     }
 
-    private void handleMarkerClick(Marker marker) {
-        LatLng markerLatLng = marker.getPosition();
-        String message = "Marker clicked! Coordinates: " + markerLatLng.latitude + ", " + markerLatLng.longitude;
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    private void handleMapClick(LatLng latLng) {
-        String message = "Map clicked! Coordinates: " + latLng.latitude + ", " + latLng.longitude;
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        // Handle map click
+        LatLng location = new LatLng(latLng.latitude, latLng.longitude);
+        Marker marker = map.addMarker(new MarkerOptions().position(location).title("marker"));
+        String country = GeoCodingHelper.getCountryFromLocation(this, location.latitude, location.longitude);
+        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();
+        // Remove marker after toast message
+        marker.remove();
     }
 }
--- app/src/main/java/com/example/googlemapstrial/MainActivity.java
+++ app/src/main/java/com/example/googlemapstrial/MainActivity.java
@@ -1,29 +1,13 @@
 package com.example.googlemapstrial;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
-
-    private GoogleMap map;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,50 +17,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-
     }
-    //Commit practice
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
-        map = googleMap;
-
-        map.setOnMapClickListener(this);
-        map.getUiSettings().setZoomControlsEnabled(true);
-
-        map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
-
-        try
-        {
-            boolean success = map.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json ) );
-
-            if( !success )
-            {
-                Log.e( TAG, "Style parsing failed." );
-            }
-        }
-        catch( Resources.NotFoundException e )
-        {
-            Log.e( TAG, "Can't find style. Error: ", e );
-        }
-
-
-
-       // map.setMapType(MapStyleOptions.loadRawResourceStyle(this, R.ra));
-
-    }
-
-
-    @Override
-    public void onMapClick(@NonNull LatLng latLng) {
-        LatLng location = new LatLng(latLng.latitude, latLng.longitude);
-        Marker marker = map.addMarker(new MarkerOptions().position(location).title("marker"));
-        String country = GeoCodingHelper.getCountryFromLocation(this, location.latitude, location.longitude);
-        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();
-        //remove marker after toast message
-        marker.remove();
-
+        //No need to implement, already within GoogleMapsActivity.
     }
 }
