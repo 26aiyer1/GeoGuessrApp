71ece68a9a157b3809e00a881d3b93d354794f4f
adhvikharikrishnan <adhvikthedude@gmail.com>
2024-04-02 17:09:58 -0700

    Revert "created intents for mainactivity and the google maps class"
    This reverts commit 03fec5bc0d7669777b2e5e130f42808b8200a45b.

--- app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
+++ app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
@@ -5,7 +5,6 @@ import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,16 +15,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.content.Intent;
-import android.widget.Button;
 
 public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap map;
-
-    Button back;
-
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +36,6 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         map.setOnMapClickListener(this);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
-        back = findViewById(R.id.back);
-
-        intent = new Intent(GoogleMapsActivity.this, MainActivity.class);
 
         try
         {
@@ -60,13 +50,6 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         {
             Log.e( TAG, "Can't find style. Error: ", e );
         }
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent);
-            }
-        });
 
     }
 
