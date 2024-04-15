2e908be36ddeccfaa5dffd57506923c0a7351b44
26aiyer1 <26aiyer1@stratfordschools.net>
2024-04-15 16:26:38 -0700

    Implemented Gemini API to generate country names for the Google Maps Game. Need to make some refinements to the XML associated with the java class, then we're good!

--- app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
+++ app/src/main/java/com/example/googlemapstrial/GoogleMapsActivity.java
@@ -2,11 +2,17 @@ package com.example.googlemapstrial;
 
 
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.java.GenerativeModelFutures;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.GenerateContentResponse;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,6 +20,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
 import android.content.Intent;
 import android.widget.Button;
 
@@ -24,6 +34,12 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
     Button back;
 
     Intent intent;
+
+    String question;
+
+    Button genQuestion;
+
+    String countryGenerated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +57,21 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         map.setOnMapClickListener(this);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
-        back = findViewById(R.id.back);
+        back = findViewById(R.id.genQuestion);
+        genQuestion = findViewById(R.id.genQuestion);
 
         intent = new Intent(GoogleMapsActivity.this, MainActivity.class);
 
         map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json ) );
 
 
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,7 +87,47 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         LatLng location = new LatLng(latLng.latitude, latLng.longitude);
         Marker marker = map.addMarker(new MarkerOptions().position(location).title("marker"));
         String country = GeoCodingHelper.getCountryFromLocation(this, location.latitude, location.longitude);
-        Toast.makeText(this, country, Toast.LENGTH_SHORT).show();
+        checkAnswer(country);
         marker.remove();
+    }
+
+
+    public void generateQuestion(){
+        GenerativeModel gm = new GenerativeModel("gemini-pro",
+                "AIzaSyArulVC_IsImpG9vRtDy-D8Gi4vOMEDZtc");
+        GenerativeModelFutures model = GenerativeModelFutures.from(gm);
+
+        Content content = new Content.Builder()
+                .addText("Generate a random country name in the following format: (Country) ")
+                .build();
+
+
+        ListenableFuture<GenerateContentResponse> response = model.generateContent(content);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Futures.addCallback(response, new FutureCallback<GenerateContentResponse>() {
+                @Override
+                public void onSuccess(GenerateContentResponse result) {
+                    question = result.getText();
+                    countryGenerated = question;
+
+                }
+                @Override
+                public void onFailure(Throwable t) {
+                    t.printStackTrace();
+                }
+            }, this.getMainExecutor());
+        }
+
+    }
+
+
+
+    public void checkAnswer(String countryClicked){
+        if(countryGenerated.toString().equals(countryClicked)){
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
