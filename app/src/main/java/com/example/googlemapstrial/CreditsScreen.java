25cb7a1a70ade9164b44f831651286d3e470b2fc
26aiyer1 <26aiyer1@stratfordschools.net>
2024-04-29 11:27:17 -0700

    Added Credits class, and with this we have completed all of the features we wished to achieve

---
+++ app/src/main/java/com/example/googlemapstrial/CreditsScreen.java
@@ -0,0 +1,14 @@
+package com.example.googlemapstrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreditsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits_screen);
+    }
+}
