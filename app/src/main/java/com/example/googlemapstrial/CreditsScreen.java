7ddff15a48da25f44b86ffc2695343cb816794e6
26aiyer1 <26aiyer1@stratfordschools.net>
2024-04-30 11:24:11 -0700

    added Back button to CreditsScreen.java

--- app/src/main/java/com/example/googlemapstrial/CreditsScreen.java
+++ app/src/main/java/com/example/googlemapstrial/CreditsScreen.java
@@ -2,13 +2,30 @@ package com.example.googlemapstrial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreditsScreen extends AppCompatActivity {
 
+    Button backFromCredits;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits_screen);
+
+        backFromCredits = findViewById(R.id.backFromCreditsButton);
+
+        Intent headBack = new Intent(CreditsScreen.this, MainActivity.class);
+
+        backFromCredits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(headBack);
+            }
+        });
     }
 }
